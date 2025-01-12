1c5facd2d076ec593b456f956c7514bd734d672b
JoramWells <jorammanoah1@gmail.com>
2025-01-12 08:25:16 +0300

    modified app

--- app/(payroll)/employee-records/add/page.tsx
+++ app/(payroll)/employee-records/add/page.tsx
@@ -1,62 +1,71 @@
-'use client';
+// 'use client';
+
+// import React from 'react';
+// import BreadcrumbNav from '@/components/custom/nav/BreadcrumbNav';
+// import InputText from '@/components/custom/forms/InputText';
+
+// const listItems = [
+//   {
+//     id: '1',
+//     label: 'home',
+//     link: '/',
+//   },
+//   {
+//     id: '2',
+//     label: 'Employee Records',
+//     link: '/employee-records',
+//   },
+//   {
+//     id: '3',
+//     label: 'Add New Employee Record',
+//     link: '',
+//   },
+// ];
+// function EmployeeRecordsDetailPage() {
+//   return (
+//     <>
+//       <BreadcrumbNav
+//         listItems={listItems}
+//       />
+
+//       <div
+//         className="p-2"
+//       >
+
+//         <div className="w-1/3 p-4 bg-white rounded-lg border flex flex-col
+// space-y-4 border-zinc-100">
+//           <div className="flex flex-col space-y-2">
+//             <p>Bank Details</p>
+//             <InputText
+//               placeholder="Select Bank"
+//             />
+//             <InputText
+//               placeholder="Bank Account No."
+//             />
+//           </div>
+
+//           <div className="flex flex-col space-y-2">
+//             <p>Insurance Details</p>
+//             <InputText
+//               placeholder="Enter NHIF No."
+//             />
+//             <InputText
+//               placeholder="Enter NSSF No."
+//             />
+//           </div>
+
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default EmployeeRecordsDetailPage;
 
 import React from 'react';
-import BreadcrumbNav from '@/components/custom/nav/BreadcrumbNav';
-import InputText from '@/components/custom/forms/InputText';
 
-const listItems = [
-  {
-    id: '1',
-    label: 'home',
-    link: '/',
-  },
-  {
-    id: '2',
-    label: 'Employee Records',
-    link: '/employee-records',
-  },
-  {
-    id: '3',
-    label: 'Add New Employee Record',
-    link: '',
-  },
-];
-function EmployeeRecordsDetailPage() {
-  return (
-    <>
-      <BreadcrumbNav
-        listItems={listItems}
-      />
+const page = () => (
+  <div>page</div>
+);
 
-      <div
-        className="p-2"
-      >
-
-        <div className="w-1/3 p-4 bg-white rounded-lg border flex flex-col space-y-4 border-zinc-100">
-          <div className="flex flex-col space-y-2">
-            <p>Bank Details</p>
-            <InputText
-              placeholder="Select Bank"
-            />
-            <InputText
-              placeholder="Bank Account No."
-            />
-          </div>
-
-          <div className="flex flex-col space-y-2">
-            <p>Insurance Details</p>
-            <InputText
-              placeholder="Enter NHIF No."
-            />
-            <InputText
-              placeholder="Enter NSSF No."
-            />
-          </div>
-
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default EmployeeRecordsDetailPage;
+export default page;
